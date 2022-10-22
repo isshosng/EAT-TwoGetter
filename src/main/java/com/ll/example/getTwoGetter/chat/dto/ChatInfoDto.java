7d4723c93d7009bb6327ba937cf24a5f48d3b417
sh970901 <aaddss639@naver.com>
2022-10-22 13:41:56 +0900

    chatInfo Entity

--- src/main/java/com/ll/example/getTwoGetter/chat/dto/ChatInfoDto.java
+++ src/main/java/com/ll/example/getTwoGetter/chat/dto/ChatInfoDto.java
@@ -1,13 +1,18 @@
 package com.ll.example.getTwoGetter.chat.dto;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatInfoDto {
+    @NotNull
     private String chatTitle;
+    @NotNull
     private String username;
+    @NotNull
     private String partner;
+    @NotNull
     private String email;
 }
